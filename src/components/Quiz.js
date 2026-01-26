5a116770ee63c9792d30f710f724d7b0dc29a513
_2gvs5bqthy <1240502@ubdc.ac.jp>
2026-01-26 02:08:32 +0000

    ver2

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -532,7 +532,7 @@ export default function Quiz({
         />
       )}
 
-      {/* ★ DebugPanel をコメントアウト */}
+      {/* ★ DebugPanel をコメントアウトしてのこしておく */}
       {/*<DebugPanel
         gameMode="main"
         questionNumber={questionNumber}
